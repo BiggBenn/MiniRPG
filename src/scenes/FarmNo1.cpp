58c18caaae3f4e9de34c5f1a1d9513167448d1a1
BiggBenn <benni.hudson@googlemail.com>
2021-05-31 22:01:03 +0200

    Implemented basic shape of level 1, subject to change

--- src/scenes/FarmNo1.cpp
+++ src/scenes/FarmNo1.cpp
@@ -23,8 +23,56 @@ void FarmNo1::Initialize()
 
 	//create colliders
 	GameObject* A1 = new GameObject();
-	A1->SetCollider({ ColliderType::Rect, {0,0,30, 1000} });
-	A1->SetSize({ 30,1000 });
-	A1->SetPosition({ -400,0 });
+	A1->SetCollider({ ColliderType::Rect, {0,0,30, 1800} });
+	A1->SetSize({ 30,1800 });
+	A1->SetPosition({ -800,0 });
 	all_objects.push_back(A1);
+
+	GameObject* A2 = new GameObject();
+	A2->SetCollider({ ColliderType::Rect, {0,0,300, 30} });
+	A2->SetSize({ 300, 30 });
+	A2->SetPosition({ -650, -900 });
+	all_objects.push_back(A2);
+
+	GameObject* A3 = new GameObject();
+	A3->SetCollider({ ColliderType::Rect, {0,0,300, 30} });
+	A3->SetSize({ 300, 30 });
+	A3->SetPosition({ -650, 900 });
+	all_objects.push_back(A3);
+
+	GameObject* A4 = new GameObject();
+	A4->SetCollider({ ColliderType::Rect, {0,0,30, 750} });
+	A4->SetSize({ 30,750 });
+	A4->SetPosition({ -500,-525 });
+	all_objects.push_back(A4);
+
+	GameObject* A5 = new GameObject();
+	A5->SetCollider({ ColliderType::Rect, {0,0,30, 750} });
+	A5->SetSize({ 30,750 });
+	A5->SetPosition({ -500,525 });
+	all_objects.push_back(A5);
+
+	GameObject* B1 = new GameObject();
+	B1->SetCollider({ ColliderType::Rect, {0,0,1500, 30} });
+	B1->SetSize({ 1500,30 });
+	B1->SetPosition({ 250,150 });
+	all_objects.push_back(B1);
+
+	GameObject* B2 = new GameObject();
+	B2->SetCollider({ ColliderType::Rect, {0,0,800, 30} });
+	B2->SetSize({ 800,30 });
+	B2->SetPosition({ -100,-150 });
+	all_objects.push_back(B2);
+
+	GameObject* B3 = new GameObject();
+	B3->SetCollider({ ColliderType::Rect, {0,0,30, 750} });
+	B3->SetSize({ 30,750 });
+	B3->SetPosition({ 300,-525 });
+	all_objects.push_back(B3);
+
+	/*GameObject* B4 = new GameObject();
+	B4->SetCollider({ ColliderType::Rect, {0,0,30, 750} });
+	B4->SetSize({ 30,750 });
+	B4->SetPosition({ 300,-525 });
+	all_objects.push_back(B4);*/
 }
