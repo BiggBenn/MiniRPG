a331c12a09bb949c3a7660775a2c85b0dc9353c8
MP <pavlik@localhost.company>
2021-06-14 00:00:37 +0200

    basic map building items and jenkins's farm example (wip)

---
+++ resources/maps/tilemap_roads.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="tilemap_roads" tilewidth="62" tileheight="62" spacing="2" margin="16" tilecount="48" columns="12">
+ <image source="tilemap_roads.png" width="798" height="286"/>
+</tileset>
